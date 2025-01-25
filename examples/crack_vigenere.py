fe79c85b5313e08a49539d3afd81860ee7ed14de
Matt Gruskin <matthew.gruskin@gmail.com>
2025-01-25 14:56:01 -0500

    lint fixes for Vigenere cipher cracking example

--- examples/crack_vigenere.py
+++ examples/crack_vigenere.py
@@ -1,19 +1,30 @@
+"""Example program to crack a Vigenere cipher."""
+
 from texput.cipher import vigenere_decrypt
 from texput.fitness import QuadgramStatistics
 from texput.mung import uppercase_only
 from texput.optimize import brute_force
 
-if __name__ == "__main__":
+
+def main():
+    """Example program to crack a Vigenere cipher."""
     candidates = set()
-    with open("/usr/share/dict/words") as f:
+    with open("/usr/share/dict/words", encoding="utf-8") as f:
         for line in f:
             word = uppercase_only(line.strip())
             if len(word) == 5:
                 candidates.add(word)
-    
+
     qs = QuadgramStatistics()
     ciphertext = "LXFOPVEFRNHR"
-    objective = lambda k: qs.string_score(vigenere_decrypt(ciphertext, k))
+
+    def objective(k):
+        return qs.string_score(vigenere_decrypt(ciphertext, k))
+
     results = brute_force(objective, candidates)
     for key, score in results:
         print(f"{key:20} {vigenere_decrypt(ciphertext, key)} {score}")
+
+
+if __name__ == "__main__":
+    main()
